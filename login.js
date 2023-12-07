e8aa8f8ad9f11c7ba704258d943cc04bd8aaaa90
Jack <cavernous.dreams@gmail.com>
2023-12-07 21:21:05 +0300

    init commit

---
+++ login.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const ejs = require("ejs");
+const path = require('path');
+const bodyParser = require("body-parser");
+
+const https = require('https');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs"); // Set EJS as the view engine
+app.engine('html', ejs.renderFile); // Tell EJS to use .html files
+app.set('views', path.join(__dirname, 'public'));
+
+
+// ========================= Organisations =========================
+
+app.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
